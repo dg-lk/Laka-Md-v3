65e3143e2cbae01176a654d86443a898013c71cd
dg-lk <denuwaralakshan634@gmail.com>
2025-07-10 08:19:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=19QHRSbS#rYs9jQq6H287fWLcQ7IvUCNy8OZGrV4nVXifhd_xvLw",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=hkBmwD7S#y75gu3yR4Tnzh-OELq4nz_ujTKrIZWrkCCN0pTfHqzQ",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
